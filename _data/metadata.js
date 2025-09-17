9fa361c61bf737e50b68b4c638ebc54f49877943
Mae Capacite <45900436+cadrianmae@users.noreply.github.com>
2025-09-17 23:06:15 +0100

    Initial Commit

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Mae's Blog for Computer Science Year 4",
+	url: "maeirl.github.io/TU856-4-Blog",
+	language: "en",
+	description: "Blog website Mae's for Computer Science Year 4",
+	author: {
+		name: "Adrian Mae",
+		email: "",
+		url: "maeirl.com",
+	}
+}
